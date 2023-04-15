2b6a97f2d5e5a65e5cfe7eac268aa255dd52a87c
nomayen <nomayen.ohin@gmail.com>
2023-04-15 12:41:01 +0600

    user create

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAl2rb5MUfnrc4RRbB4-rCySEj3zbg95BI",
+  authDomain: "aircnc-3145a.firebaseapp.com",
+  projectId: "aircnc-3145a",
+  storageBucket: "aircnc-3145a.appspot.com",
+  messagingSenderId: "976527151877",
+  appId: "1:976527151877:web:6466f82b755f26378cdd6c"
+};
+
+const app = initializeApp(firebaseConfig)
+
+export default app
